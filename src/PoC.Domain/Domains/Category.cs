345ae6cc6f884b3fa875b56c3129383e8cb6138a
Oguzcan Genc <oguzcangencc@hotmail.com>
2023-12-24 00:46:33 +0300

    Remove redundant .idea files for PoC.EntityFramework
    ## Changes Summary
    
    ### Deleted Files:
    1. **File:** `.idea/.idea.PoC.EntityFramework/.idea/.gitignore`
       - Removed default and Rider-specific ignored files.
    
    2. **File:** `.idea/.idea.PoC.EntityFramework/.idea/encodings.xml`
       - Deleted file related to encoding configurations.
    
    3. **File:** `.idea/.idea.PoC.EntityFramework/.idea/indexLayout.xml`
       - Removed `indexLayout.xml` file which contained user content model configurations.
    
    These files were deleted, likely removing specific configurations and ignored files within the project's `.idea` directory related to the PoC.EntityFramework.
    
    ## Commit Message
    "Remove redundant .idea files for PoC.EntityFramework"

---
+++ src/PoC.Domain/Domains/Category.cs
@@ -0,0 +1,14 @@
+﻿using Common.Entity;
+
+namespace PoC.Domain.Domains
+{
+    public class Category(string name, string description) : BaseEntity<Guid>
+    {
+        public string Name { get; set; } = name;
+
+        public string Description { get; set; } = description;
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ src/Shared/Common/Entity/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Entity
+{
+    public interface IEntity<T>
+    {
+        [Key]
+        T Id { get; set; }
+
+        bool IsDeleted { get; set; }
+    } 
+   
+}
